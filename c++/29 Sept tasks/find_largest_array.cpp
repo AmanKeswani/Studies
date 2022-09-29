48ee18d9f35b43cd03a9305fd0fc5c655e56adc8
DeltaCoderr <subodhinamdar99@gmail.com>
2022-09-29 09:52:24 +0530

    Add new task

---
+++ c++/29 Sept tasks/find_largest_array.cpp
@@ -0,0 +1,28 @@
+// write a program to read an array and find the largest of them
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter number of elements of array" << endl;
+    int n;
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements of array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int largest = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+        cout << "Largest = " << largest << endl;
+        return 0;
+    }
+
