5f8b0c508a59cafd2e5ae81d6efe3001e77d2496
kesawniaman <keswaniaman137@gmail.com>
2022-11-08 18:52:32 +0530

    8 Nov

---
+++ WebTechnologies/8_Nov/switch.js
@@ -0,0 +1,12 @@
+let day = "Tuesday"
+
+switch(day) {
+    case "Monday":
+        console.log("Monday");
+        break;
+    case "Tuesday":
+        console.log("Tuesday");
+        break;
+    default : 
+    day = "rest day"
+}  
